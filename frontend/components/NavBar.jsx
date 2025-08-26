0f91e8722dac91298de3a9dcf011beb1d01abf22
Richa Gupta <richamgupta2005@gmail.com>
2025-08-26 16:05:14 +0530

    navBar

--- frontend/components/NavBar.jsx
+++ frontend/components/NavBar.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
+    <nav className="navbar">
+
+      <div className="navbar-container">
+        <div className="navbar-logo">
+          
+          <Link to="/"><img src ="{/* logo path*/}"/></Link>
+
+        </div>
+
+        <div className="nav-links">
+          <button className="nav-item">
+            <Link to="/" className="nav-link">Home</Link> 
+          </button> <br />
+          <button className="nav-item">
+            <Link to="/activity" className="nav-link">Activity</Link>
+          </button> <br />
+          <button className="nav-item">
+            <Link to="/account" className="nav-link">Account</Link>
+          </button> <br />
+
+        </div>
+      </div>
+    </nav>
   )
 }
 
