cc4cc8a76748d38d9791415d246cd31b7e5a737a
Richa Gupta <richamgupta2005@gmail.com>
2025-08-26 13:24:55 +0530

    Setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,21 @@
-
-function App() {
+import react from 'react'
+import {Routes, Route} from 'react-router-dom'
+import HomePage from '../pages/homePage'
+import Activity from '../pages/activity'
+import Account from '../pages/account'
+import NavBar from '../components/NavBar'
+export default function App() {
+  return(
+  <>
+    <NavBar />
+    <Routes>
+      <Route path= '/' element={<HomePage />} />
+      <Route path= '/activity' element={<Activity />} />
+      <Route path = '/account' element={<Account />} />
+    </Routes>
+  </>
+  )
   
 }
 
-export default App
+
