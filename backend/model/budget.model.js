f4b4ea3e26f416a45cbfa7815922ad993418700b
Richa Gupta <richamgupta2005@gmail.com>
2025-08-27 00:14:27 +0530

    Initial Backend

---
+++ backend/model/budget.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose"
+
+const budgetSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',      
+        required: true
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      description: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      need: {
+        type: String,
+        required: true,
+        enum: ['low', 'medium', 'high'], 
+      },
+      date: {
+        type: Date,
+        required: true,
+        default: Date.now, 
+      }
+
+}, {
+    timestamps:true  
+})
+
+const Budget= mongoose.model('Budget',budgetSchema)
+
+export default Budget;
