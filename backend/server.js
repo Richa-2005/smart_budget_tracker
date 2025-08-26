feb40dd611c250fa5f8a068e033d96b775a34382
Richa Gupta <richamgupta2005@gmail.com>
2025-08-27 01:21:08 +0530

    Backend bioler plate

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import {connectDB} from './config/db.js'
+import budgetRoutes from './routes/budget.routes.js'
+import userRoutes from './routes/user.routes.js'
+
+const app = express()
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, ()=>{
+    connectDB()
+    console.log(`Server started listening at port 3000 : http://localhost:${PORT}/`)
+})
+
+app.use(express.json())
+
+app.use('/user',userRoutes)
+
+app.use('/budget',budgetRoutes)
